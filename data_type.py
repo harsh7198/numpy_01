43b8f5cfe6fd0b858bdd8b3397c748bbd233c9a7
harsh7198 <harshsuradkar2221@gmail.com>
2026-01-17 10:10:11 +0530

    Change Datatype

---
+++ data_type.py
@@ -0,0 +1,10 @@
+import numpy as np 
+# data type change 
+D = np.array([[1,2,3],[4,5,6]], dtype=float)
+S = np.array([[1,2,3],[4,5,6]], dtype=str)
+P = np.array([[1,2,3],[4,5,6]], dtype=bool)
+Q = np.array([[1,2,3],[4,5,6]], dtype=complex)
+print(D)
+print(S)
+print(P)
+print(Q)
