3b1626ec49a60dd36e4d8eac51beb9dac0d97d8a
harsh7198 <harshsuradkar2221@gmail.com>
2026-01-26 16:23:28 +0530

    Indexing of 1D

---
+++ index_slicing/indexing_01.py
@@ -0,0 +1,9 @@
+import numpy as np 
+
+
+# index start from 0 
+# means access specific element 
+s = np.array([1,2,3,4,5])
+print(s[0])
+print(s[-1]) # last element of array 
+print(s[1:]) # 1 to all element 
