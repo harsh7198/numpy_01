876166546651d3ea14c22ce2146fa2af2606c48f
harsh7198 <harshsuradkar2221@gmail.com>
2026-01-26 12:01:06 +0530

    relational & logical

---
+++ array_operations/relational_opr.py
@@ -0,0 +1,13 @@
+import numpy as np 
+
+x = np.array([[1,2,3],[4,5,6],[7,8,9]])
+print(x>=2)
+print("\n")
+print(x==3)
+
+# logical operation 
+
+a = np.array([10, 20, 30])
+
+print((a > 15) & (a < 30))
+print((a < 15) | (a > 25))
