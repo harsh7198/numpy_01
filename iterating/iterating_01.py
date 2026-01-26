7b9ff7aae73ca3153189921f2e2ff078882afdb1
harsh7198 <harshsuradkar2221@gmail.com>
2026-01-26 17:26:24 +0530

    Iterating using loop

---
+++ iterating/iterating_01.py
@@ -0,0 +1,17 @@
+import numpy as np 
+# 1D array 
+s = np.array([1,2,3,4,5,6,7])
+
+for x in s:
+    print(x)
+
+# 2D array
+S = np.array([[1,2,3],[4,5,6]])
+
+for X in S : 
+    print(X)
+
+# 3D array 
+e = np.array([[[1,2,3],[4,5,6]],[[7,8,9],[10,11,12]]])
+for i in e :
+    print(i)
