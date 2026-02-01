c2e28973d4447b9b6b9c797df8aa315963d7a217
harsh7198 <harshsuradkar2221@gmail.com>
2026-02-01 20:00:09 +0530

    Ravel and Tranpose

---
+++ iterating/ravel_function.py
@@ -0,0 +1,12 @@
+import numpy as np 
+
+# ravel() convert any array to 1D 
+
+s = np.array([[1,2,3],[4,5,6]])
+print(np.ravel(s))
+
+
+# tranpose the array 
+
+d = np.array([[1,2,3] , [4,5,6]])
+print(np.transpose(d))
